fc082dbbf852c35c883789bfd0fa2cccc0f2a123
olzzon <github@olzzon.dk>
2025-01-16 12:46:59 +0100

    DEBUG: only debug Ember+ S101Codec

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -3,5 +3,5 @@ import { DefaultLogger } from '@tv2media/logger/node'
 export const logger = new DefaultLogger()
 // Enable debug logging for the Ember+ connection
 // The DEBUG needs to be set after the logger is created:
-process.env.DEBUG = 'emberplus-connection:*'
+process.env.DEBUG = 'emberplus-connection:S101Codec'
 
